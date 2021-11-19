c210ba4f1d114b6524d88552e01e4182b1c0713b
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2021-11-18 23:07:58 -0500

    Added An Input System

---
+++ Architect Game Engine/src/Input System/Input.h
@@ -0,0 +1,49 @@
+#pragma once
+
+namespace Architect
+{
+	enum class KeyCode
+	{
+		A, B, C, D, E, F, G, H,
+		I, J, K, L, M, N, O, P, 
+		Q, R, S, T, U, V, W, X, 
+		Y, Z,
+
+		UpArrow, DownArrow,
+		LeftArrow, RightArrow,
+
+		LeftShift, RightShift,
+
+		LeftContol, RightContol,
+
+		LeftAlt, RightAlt,
+
+		Space, Tab, CapsLock,
+
+		Alfa1, Alfa2, Alfa3, Alfa4, Alfa5,
+		Alfa6, Alfa7, Alfa8, Alfa9, Alfa0
+	};
+
+	enum class MouseButtonCode
+	{
+		Left,
+		Right,
+		Middle
+	};
+
+	class Input
+	{
+	public:
+		static bool GetKey(KeyCode keycode); // checks if a key is being pressed
+		static bool GetKeyDown(KeyCode keycode); // checks if a key has been pressed this frame
+		static bool GetKeyUp(KeyCode keycode); // checks if a key has been releced
+
+		static bool GetMouseButton(MouseButtonCode buttonCode); // checks if a mouse button is being pressed
+	    static bool GetMouseButtonDown(MouseButtonCode buttonCode); // checks if a mouse button has been pressed this frame
+		static bool GetMouseButtonUp(MouseButtonCode buttonCode); // checks if a mouse button has been releced
+
+		static float GetMouseScrollWheelDelta();
+	};
+
+}
+
--- Architect Game Engine/src/Main.cpp
+++ Architect Game Engine/src/Main.cpp
@@ -2,10 +2,12 @@
 #include "WindowsDefines.h" // fixes nameing issues with windows.h and raylib, also must be included after raylib
 #include "Logger/Logger.h"
 
+#include "Input System/Input.h"
+
 void LogCustom(int msgType, const char* text, va_list args)
 {
     int length = _vscprintf(text, args) + 1; // _vscprintf doesn't count the terminating '\0'
-    char* buffer = (char*)malloc(length * sizeof(char));
+    char* buffer = new char[length];
 
     if (buffer != NULL)
     {
@@ -19,14 +21,16 @@ void LogCustom(int msgType, const char* text, va_list args)
         case LOG_DEBUG: ARC_ENGINE_TRACE(buffer); break;
         default: break;
         }
-
-        free(buffer);
     }
+
+    delete(buffer);
 }
+
+using namespace Architect;
 
 int main(void)
 {
-    Architect::Logger::Init();
+    Logger::Init();
 
     // Initialization
     //--------------------------------------------------------------------------------------
@@ -48,6 +52,11 @@ int main(void)
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
 
+        if (Input::GetKeyUp(KeyCode::Alfa0))
+        {
+            ARC_INFO("0 Key Pressed");
+        }
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
@@ -57,6 +66,7 @@ int main(void)
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
+
         //----------------------------------------------------------------------------------
     }
 
